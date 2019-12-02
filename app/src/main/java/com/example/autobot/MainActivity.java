dc55fa870b2f8212084b13631f86c00ac13c4391
akevli <akli2@illinois.edu>
2019-12-02 13:25:27 -0600

    Add SmsManager API

--- app/src/main/java/com/example/autobot/MainActivity.java
+++ app/src/main/java/com/example/autobot/MainActivity.java
@@ -11,12 +11,42 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Telephony;
+import android.support.v4.app.*;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
+    private EditText txtMobile;
+    private EditText txtMessage;
+    private Button btnSms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtMobile = (EditText)findViewById(R.id.mblTxt);
+        txtMessage = (EditText)findViewById(R.id.msgTxt);
+        btnSms = (Button)findViewById(R.id.btnSend);
+        btnSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    SmsManager smgr = SmsManager.getDefault();
+                    smgr.sendTextMessage(txtMobile.getText().toString(),null,txtMessage.getText().toString(),null,null);
+                    Toast.makeText(MainActivity.this, "SMS Sent Successfully", Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception e){
+                    Toast.makeText(MainActivity.this, "SMS Failed to Send, Please try again", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         /*Button button = (Button) findViewById(R.id.Enter);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,15 +63,7 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         s.setAdapter(adapter);
 
-        Button send = findViewById(R.id.Send);
-        send.setOnClickListener(v -> {
-            smsFunction();
-        });
-    }
-    private void smsFunction() {
-        EditText message = findViewById(R.id.EnterMessage);
-        if (!(message.getText().toString().equals(""))) {
-        }
+
     }
     /*private void LaunchMainAction() {
         Intent intent = new Intent(this, MainAction.class);
